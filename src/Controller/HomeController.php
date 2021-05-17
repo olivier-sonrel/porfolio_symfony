92e2c05eec51b9f3cbf89ea181640a9a26e21931
olivier-sonrel <o.sonrel@it-students.fr>
2021-05-17 16:59:13 +0300

    modif gitignore

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/pro", name="pro")
+     */
+    public function pro(): Response
+    {
+        return $this->render('home/pro.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/hobbies", name="hobbies")
+     */
+    public function hobbies(): Response
+    {
+        return $this->render('home/hobbies.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
